69cf22434ea315f4a7260cc6771e0b1a56eb701d
Kiran <kiranspatil3663@gmail.com>
2022-12-03 15:24:53 +0530

    ScoreCalculator updated

--- auti_app/views.py
+++ auti_app/views.py
@@ -4,13 +4,17 @@ from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 from django.http import JsonResponse
 from fuzzywuzzy import fuzz
+from pyphonetics import RefinedSoundex
 
 @api_view(['GET'])
 def ScoreCalculator(request):
     
     UserInput = request.GET.get('user')
     StandardInput = request.GET.get('standard')
-    results = fuzz.partial_ratio(UserInput,StandardInput)
-    return JsonResponse({'results':results})
+    results = fuzz.ratio(UserInput,StandardInput)
+    rs = RefinedSoundex()
+    ans = rs.distance(UserInput.lower(),StandardInput.lower())
+
+    return JsonResponse({'results':results,'Answer':ans})
 
 # Create your views here.
